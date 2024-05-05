69237a7e83ceb995f82ccbc6043dad5bceb9d84e
Gabriel Paiva <gabrielpaiva@gmail.com>
2024-05-05 11:01:55 -0500

    first commit

---
+++ src/main/java/pe/edu/utp/apiloggin/ApiLogginApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.apiloggin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLogginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLogginApplication.class, args);
+    }
+
+}
